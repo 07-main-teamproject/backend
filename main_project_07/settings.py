dd1bac1ec456ebcf099250a4d31485a3f6152131
Kiyeoun <agumon70@naver.com>
2025-02-13 16:56:42 +0900

    📝 Profile image add
    # Conflicts:
    #	main_project_07/settings.py

--- main_project_07/settings.py
+++ main_project_07/settings.py
@@ -133,13 +133,14 @@ REST_FRAMEWORK = {
     ),
 }
 
+
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",  # 로컬 메모리 캐싱
         "LOCATION": "unique-snowflake",  # 캐시 구분을 위한 위치명
     }
 }
-
 
 AUTH_USER_MODEL = 'user.User'
 
@@ -151,4 +152,7 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
