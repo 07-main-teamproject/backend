0bfed107311acca1c7ee607639ac569cd4f36703
SOLBI <thfqlsla35@gmail.com>
2025-02-15 15:46:38 +0900

    💡 Diet/urls.py 수정
    식단 대량삭제 부분 삭제함

--- diet/urls.py
+++ diet/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', DietListView.as_view(), name='diet-list'),  # 사용자의 식단 전체 목록 조회
     path('<int:diet_id>/', DietDetailView.as_view(), name='diet-detail'),  # 단일 조회 추가
     path('create/', DietCreateView.as_view(), name='diet-create'),  # 새로운 식단 생성
-    path('delete/', DietDeleteView.as_view(), name='diet-bulk-delete'),  # 대량 삭제 (쿼리 없는 경우)
     path('delete/<int:diet_id>/', DietDeleteView.as_view(), name='diet-delete'),  # 단일 삭제
 
 ]
